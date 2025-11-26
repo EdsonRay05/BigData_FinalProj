f10eebf21ae673ea145418807183bb082f3cd8c4
EdsonRay05 <edsonsanjuan13@gmail.com>
2025-11-26 11:29:25 +0800

    Refactor MongoDB URI in mongodb_sup.py
    Updated MongoDB URI placeholder for clarity.

--- mongodb_sup.py
+++ mongodb_sup.py
@@ -9,7 +9,7 @@ import json
 KAFKA_BROKER = 'localhost:9092'                 # Your Kafka broker address
 TOPIC = 'weather_topic'                         # Kafka topic to listen to
 # MongoDB connection URI (replace with your own!)
-MONGO_URI = 'mongodb+srv://qeresanjuan:nosdeyar@cluster0.sgf8rsf.mongodb.net/?appName=Cluster0'
+MONGO_URI = # Replace with your mongodb uri
 DB_NAME = 'weather_db'                          # Database name in MongoDB
 COLLECTION_NAME = 'weather_data'                # Collection inside DB
 
@@ -32,4 +32,5 @@ print("Consumer started, saving data to MongoDB...")
 for message in consumer:
     data = message.value
     collection.insert_one(data)
+
     print("Saved to MongoDB:", data)
